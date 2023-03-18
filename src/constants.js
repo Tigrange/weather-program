46e05ce6a10a14091c58d40ef04280412880e016
Tigrange <tikomelq@gmail.com>
2023-03-18 19:10:05 +0400

    Lazy load

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://dataservice.accuweather.com";
-export const AcuWeather_api_key = "CIGcH0J2AxqhzMsetFVLVPHlbncvsQCA";
+export const AcuWeather_api_key = "DLAEOF7eAEZgowonvPTmNnDsRGGmdAVG";
 export const IpGeolocation_api_key = "61af8e948f1a4c61a6f75d99b96e697c";
